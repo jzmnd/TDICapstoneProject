d00d693d9214053d6405937d605789c936918c11
Jeremy Smith <j.smith.03@cantab.net>
2017-11-01 00:19:35 +0000

    Work on models

--- wlmetrics.py
+++ wlmetrics.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 """
 wlmetrics.py
-Activity times lib
+Work life balance metric functions
 
 Created by Jeremy Smith on 2017-10-12
 j.smith.03@cantab.net
@@ -11,19 +11,13 @@ import numpy as np
 import pandas as pd
 
 
-# Metric 1: Weighted sum(life) - sum(work) normalized to (0,1)
+# Metric 1 and 2: Weighted sum(life) - sum(work) normalized to (0,1)
 def w_l_balance_weighted_ratio(df, pos, neg, weights_p, weights_n):
     df_p = df[['t' + a for a in pos]]
     df_n = df[['t' + a for a in neg]]
     wl = df_p.dot(weights_p) - df_n.dot(weights_n)
     wl -= wl.min()
     wl /= wl.max()
-    return wl
-
-
-# Metric 2: More than 10h work in the day
-def w_l_balance_workday(df, workid='0501', hours=10):
-    wl = df['t' + workid] > hours * 60
     return wl
 
 
@@ -43,8 +37,14 @@ def w_l_balance_leisuresocial(df):
     return wl
 
 
-# Metric 5: More than 8h childcare and/or housework
-def w_l_balance_housework(df, hours=5):
+# Metric 5: More than x hours work in the day
+def w_l_balance_workday(df, workid='0501', hours=9):
+    wl = df['t' + workid] > hours * 60
+    return wl
+
+
+# Metric 6: More than 4h childcare and/or housework
+def w_l_balance_housework(df, hours=4):
     workids = ['0301', '0302', '0303', '0201', '0202', '0209']
     workids_c = ['t' + a for a in workids]
     wl = df[workids_c].sum(axis=1) > hours * 60
